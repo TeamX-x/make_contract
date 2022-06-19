f0d2fa1728c32ad706fa892ef48f8bb1f9c513ba
hieutk <trankimhieu99@gmail.com>
2022-06-19 16:04:02 +0700

    update command build cargo

--- templates/loan-v2/build.sh
+++ templates/loan-v2/build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
 
-RUSTFLAGS='-C link-arg=-s' cargo build --target wasm32-unknown-unknown --release
+#RUSTFLAGS='-C link-arg=-s' cargo build --target wasm32-unknown-unknown --release
+RUSTFLAGS='-C link-arg=-s' /root/.cargo/bin/cargo build --target wasm32-unknown-unknown --release
 mkdir -p out
 cp target/wasm32-unknown-unknown/release/*.wasm out/loan-v2.wasm
